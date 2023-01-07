32483b12b3bfd2270554e50441dcdc6fdc0acf28
fae.exe <fae.exe@protonmail.com>
2023-01-07 16:41:21 +0100

    Screamer FOV LERP

--- arte_screamer/Assets/ScreamerController.cs
+++ arte_screamer/Assets/ScreamerController.cs
@@ -17,6 +17,23 @@ public class ScreamerController : MonoBehaviour
 
     public float minHorPos;
     public float maxHorPos;
+
+    public float minFOV;
+    public float maxFOV;
+
+    public float XOffset;
+    public float YOffset;
+
+    public float targetFOV;
+    public float velocityFOV;
+    public float smoothTimeFOV;
+
+    public float bobUpDownRatio;
+
+    public Camera CharacterCam;
+
+    public LoopAnimator motionAnimator;
+
     float Mapping(float value, float leftMin, float leftMax, float rightMin, float rightMax)
     {
         value = Mathf.Min(leftMax, value);
@@ -29,24 +46,37 @@ public class ScreamerController : MonoBehaviour
     void Start()
     {
         characterXPosition = transform.position.x;
+        motionAnimator.LaunchAnim();
     }
 
     // Update is called once per frame
     void Update()
     {
         Cursor.visible = false;
+        motionAnimator.Update();
 
         targetPosition = new Vector3(
             characterXPosition,
-            Mapping(Input.mousePosition.y, 0, Screen.height, minVertPos, maxVertPos),
+            Mapping(Input.mousePosition.y + (Screen.height/ bobUpDownRatio) * (motionAnimator.value - 0.5f), 0, Screen.height, minVertPos, maxVertPos),
             Mapping(Input.mousePosition.x, 0, Screen.width, maxHorPos, minHorPos)
         );
+
+        XOffset = Mathf.Abs(Input.mousePosition.x - Screen.width / 2) / (Screen.width / 2);
+        YOffset = Mathf.Abs(Input.mousePosition.y - Screen.height / 2) / (Screen.height / 2);
+        targetFOV = -Mapping(XOffset + YOffset, 0, 2, -maxFOV, -minFOV);
+        CharacterCam.fieldOfView = Mathf.SmoothDamp(CharacterCam.fieldOfView, targetFOV, ref velocityFOV, smoothTimeFOV);
 
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, movementSmoothTime);
 
         if (Input.GetKey(KeyCode.Mouse0))
         {
+            motionAnimator.ResumeAnim();
             characterXPosition += characterSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            motionAnimator.PauseAnim();
         }
     }
 }
