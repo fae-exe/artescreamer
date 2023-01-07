852977a299b3f40d6c90d0c91656e59c26386717
fae.exe <fae.exe@protonmail.com>
2023-01-07 15:55:03 +0100

    FIXED THE CONTROLLER FUCKING HELL

--- arte_screamer/Assets/ScreamerController.cs
+++ arte_screamer/Assets/ScreamerController.cs
@@ -6,30 +6,47 @@ public class ScreamerController : MonoBehaviour
 {
     public Vector3 targetPosition;
     public Vector3 velocity = Vector3.zero;
-    public float characterDepthPosition;
+
+    public float characterXPosition;
 
     public float movementSmoothTime = 0.3f;
     public float characterSpeed = 5.0f;
-    
-    public Vector2 worldMousePosV3 = new Vector2();
+
+    public float minVertPos;
+    public float maxVertPos;
+
+    public float minHorPos;
+    public float maxHorPos;
+    float Mapping(float value, float leftMin, float leftMax, float rightMin, float rightMax)
+    {
+        value = Mathf.Min(leftMax, value);
+        value = Mathf.Max(leftMin, value);
+
+        return rightMin + (value - leftMin) * (rightMax - rightMin) / (leftMax - leftMin);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        characterDepthPosition = transform.position.z;
+        characterXPosition = transform.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        worldMousePosV3 = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, characterDepthPosition));
-        targetPosition = new Vector3(worldMousePosV3.x, worldMousePosV3.y, characterDepthPosition);
+        Cursor.visible = false;
+
+        targetPosition = new Vector3(
+            characterXPosition,
+            Mapping(Input.mousePosition.y, 0, Screen.height, minVertPos, maxVertPos),
+            Mapping(Input.mousePosition.x, 0, Screen.width, maxHorPos, minHorPos)
+        );
 
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, movementSmoothTime);
 
         if (Input.GetKey(KeyCode.Mouse0))
         {
-            characterDepthPosition -= characterSpeed * Time.deltaTime;
+            characterXPosition += characterSpeed * Time.deltaTime;
         }
     }
 }
